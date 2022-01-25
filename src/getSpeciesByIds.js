9a2c7ec9a8559fc7abf7add20630d23c810967e2
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-01-25 15:11:53 -0300

    Cumprindo o Primeiro Requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui 
+/* Usei como referência o método includes() para checar se o elemento da array species possui o id utilizado como parâmetro. Além disso, utilizei o parâmetro rest para a função receber um número ilimitado de ids. Link https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/includes */
+
+function getSpeciesByIds(...ids) {
+  return data.species.filter((specie) => [...ids].includes(specie.id));
 }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
