78d74391d2aa305aa757bb4f975f383809693566
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-01-25 17:25:02 -0300

    Cumprindo o Terceiro Requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+/* console.log(data.employees[0].firstName); */
+/* Não usei  filter porque vai me retornar uma array, sendo que a questão pede um objeto */
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const array = data.employees;
+  return array.find((emp) => emp.firstName === employeeName || emp.lastName === employeeName);
 }
+console.log(getEmployeeByName('Wishart'));
 
 module.exports = getEmployeeByName;
