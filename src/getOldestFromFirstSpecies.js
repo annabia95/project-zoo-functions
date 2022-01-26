2b207af349622cf0003e8e44639cbaef8055a3f1
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-01-26 15:17:25 -0300

    Cumprindo o Quato Requisito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -5,3 +5,9 @@ function getOldestFromFirstSpecies(id) {
 }
 
 module.exports = getOldestFromFirstSpecies;
+/* 
+A função busca por informações do animal mais velho da primeira espécie gerenciada pela pessoa colaboradora do parâmetro.
+
+O que será avaliado
+
+Passado o id de um funcionário, encontra a primeira espécie de animal gerenciado pelo funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie. */
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
+/* Usei some para retornar um valor booleano se ao menos um elemento de um array manager satisfaça a condição de ser gerente (Ser gerente é não ter niguem como gerente). Includes permite checar se o id utilizado como parâmetro está presente no array. Se sim, retorna true, se não retorna false */
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
+/* console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
+console.log(isManager('burlId'));
+console.log(isManager('stephanieId')); */
+
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+/* Quando for false (!), temos que: */
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } /* Quando verdadeiro, utilizo filter para obter a minha array com as pessoas administradas pelo meu manageId e posteriormente utilizo map para criar uma nova array somente com o nome das pessoas que ela é responsável */
+  const filterId = data.employees.filter((employee) => (employee.managers.includes(managerId)));
+  console.log(filterId);
+  return filterId.map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
