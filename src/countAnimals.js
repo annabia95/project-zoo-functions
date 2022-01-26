da5a59db7873a925df9d4b4515f845bb86321e02
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-01-26 17:11:58 -0300

    Cumprindo o Quinto Requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  /* Meu primeiro if satisfaz a primeira condição! Quando não tenho parâmetro, retorno um objeto com a espécie do animal como chave e a sua quantidade como valor */
+  if (!animal) {
+    return data.species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  /* Caso o animal seja informado, devo retornar a sua quantidade. Para armazernar essa array somente com as informações do respectivo animal, utilizo o find */
+  const myAnimal = data.species.find((specie) => specie.name === animal.specie);
+  /* Caso seja informado o sexo, utilizo o filter nesse novo array para retornar a quantidade de animais do sexo informado. */
+  if (animal.sex) {
+    return myAnimal.residents.filter((resident) => resident.sex === animal.sex).length;
+  }
+  /* Caso não seja informado o sexo, retorno somente a quantidade de animais daquela espécie */
+  return myAnimal.residents.length;
 }
-
+console.log(countAnimals());
+console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
